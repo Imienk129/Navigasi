2398d5f73eebb3a54d69125509c5074f69b5b5eb
Jufri <imienk90@gmail.com>
2025-05-25 21:37:27 +0700

    OnPressNavigasi

--- lib/pade_dua/page_dua.dart
+++ lib/pade_dua/page_dua.dart
@@ -5,9 +5,11 @@ class PageDua extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Page Dua")),
-      body: Center(child: Text("Ini Page Dua", style: TextStyle(fontSize: 50))),
+      body: Center(child: Text("Ini Page 2", style: TextStyle(fontSize: 50))),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
         child: Icon(Icons.keyboard_arrow_left),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
--- lib/page_satu/page_satu.dart
+++ lib/page_satu/page_satu.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import '../pade_dua/page_dua.dart';
 
 class PageSatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Page Satu")),
-      body: Center(
-        child: Text("Ini Page Satu", style: TextStyle(fontSize: 50)),
-      ),
+      body: Center(child: Text("Ini Page 1", style: TextStyle(fontSize: 50))),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                return PageDua();
+              },
+            ),
+          );
+        },
         child: Icon(Icons.keyboard_arrow_right),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
